a26f33936143dba2e026539b0610d25037547938
Ivan Tubert-Brohman <itub@cpan.org>
2004-08-06 23:48:23 +0000

    Import of ITUB/Chemistry-File-SMILES-0.42 from CPAN.
    gitpan-cpan-distribution: Chemistry-File-SMILES
    gitpan-cpan-version:      0.42
    gitpan-cpan-path:         ITUB/Chemistry-File-SMILES-0.42.tar.gz
    gitpan-cpan-author:       ITUB
    gitpan-cpan-maturity:     released

--- write.pl
+++ write.pl
@@ -4,9 +4,9 @@ use blib;
 use Chemistry::File::SMILES;
 use Chemistry::File::MDLMol;
 
-my $mol = Chemistry::Mol->parse($ARGV[0] || 'CCC', format => 'smiles',
-    kekulize => $kekulize);
-#my $mol = Chemistry::Mol->read($ARGV[0] || die "pls give a .mol\n");
+#my $mol = Chemistry::Mol->parse($ARGV[0] || 'CCC', format => 'smiles',
+    #kekulize => $kekulize);
+my $mol = Chemistry::Mol->read($ARGV[0] || die "pls give a .mol\n");
 
 printf "%s (%s)\n", $mol->name, $mol->formula;
 $_->printf("%s H%h V%v\n") for $mol->atoms;
